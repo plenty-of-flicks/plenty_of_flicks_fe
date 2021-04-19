76a0e0575af4d30ce17109cadd5ae2aa4e41f84e
Nick King <nickmaxking@gmail.com>
2021-04-18 20:58:16 -0600

    Add  poro, service method and facade method for update movie details call

--- app/services/admin_service.rb
+++ app/services/admin_service.rb
@@ -1,9 +1,14 @@
-class RefreshService
+class AdminService
   def self.refresh_availability(service)
     response = conn.get('/api/v1/services/update_availability') do |req|
       req.params[:service] = service
     end
-    # response = conn.get('/api/v1/services')
+
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.update_details
+    response = conn.get('/api/v1/movies/populate_details')
 
     json = JSON.parse(response.body, symbolize_names: true)
   end
