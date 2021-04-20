d6891e69f5a51eab7a182748f6bdff66d2f7822c
Nick King <nickmaxking@gmail.com>
2021-04-20 14:36:37 -0600

    Add routes for omniauth login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  # oauth authorization routes
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/google_oauth2', as: :google_login
+  get '/logout', to: 'sessions#destroy', as: :google_logout
+
   # admin routes
   get '/admin/refresh_availability', to: 'admin#refresh_availability', as: :refresh_availability
   get '/admin/update_movie_details', to: 'admin#update_movie_details', as: :update_details
