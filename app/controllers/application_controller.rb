38f0bac3c465a17bcb06716596c66986b2a2b7e1
Nick King <nickmaxking@gmail.com>
2021-04-20 15:15:00 -0600

    Add authenticate and current_user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def authenticate
+    redirect_to google_login_path unless user_signed_in?
+  end
+
+  def current_user
+    @current_user ||= UserFacade.find_existing_user(session[:uid]) if session[:uid]
+  end
+
+  def user_signed_in?
+    !!current_user
+  end
 end
