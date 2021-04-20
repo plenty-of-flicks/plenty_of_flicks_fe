e0e104ded18f63c57f0a22464986921e342c646e
Nick King <nickmaxking@gmail.com>
2021-04-20 15:14:06 -0600

    Create route, controller action and view for profile show

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   get 'auth/google_oauth2', as: :google_login
   get '/logout', to: 'sessions#destroy', as: :google_logout
 
+  # user profile
+  get '/profile', to: 'profile#show', as: :profile
+
   # admin routes
   get '/admin/refresh_availability', to: 'admin#refresh_availability', as: :refresh_availability
   get '/admin/update_movie_details', to: 'admin#update_movie_details', as: :update_details
