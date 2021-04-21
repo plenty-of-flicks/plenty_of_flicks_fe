0bad5f368b0847c113a5085d38da71c4f772f8f9
Nick King <nickmaxking@gmail.com>
2021-04-20 19:37:36 -0600

    Add tests for unauthorized redirects for profile edit and update pages

--- spec/features/profile/edit_spec.rb
+++ spec/features/profile/edit_spec.rb
@@ -37,6 +37,20 @@ describe 'profile edit page' do
     expect(page).to have_css("img[src*='https://lh6.googleusercontent.com/-hEH5aK9fmMI/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucntLnugtaOVsqmvJGm89fFbDJ6GaQ/s96-c/photo.jpg']")
   end
 
+  it 'profile edit page redirects to welcome page if user is not logged in' do
+    visit profile_edit_path
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("You must be logged in to perform that action. Click 'Log In' to log in or create an account.")
+  end
+
+  it 'profile update page redirects to welcome page if user is not logged in' do
+    visit profile_update_path
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("You must be logged in to perform that action. Click 'Log In' to log in or create an account.")
+  end
+
   def stub_omniauth
     OmniAuth.config.test_mode = true
     OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
