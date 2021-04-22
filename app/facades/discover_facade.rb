c2612445146db26eff9bdb0eea0265426d813fb9
Nick King <nickmaxking@gmail.com>
2021-04-22 13:30:07 -0600

    Refactor services into new backend service

--- app/facades/discover_facade.rb
+++ app/facades/discover_facade.rb
@@ -1,6 +1,6 @@
 class DiscoverFacade
   def self.find_random_movie(user_id)
-    movie_info = DiscoverService.find_random_movie_info(user_id)
+    movie_info = BackendService.find_random_movie_info(user_id)
     movie = Movie.new(movie_info)
   end
 end
--- app/services/pof_be_service.rb
+++
@@ -1,5 +0,0 @@
-class PofBeService
-  def self.conn
-    Faraday.new(url: ENV['POF_BE'])
-  end
-end
