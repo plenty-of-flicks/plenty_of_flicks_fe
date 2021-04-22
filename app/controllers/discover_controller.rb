7535e69cc6aabd3bd1c962216fa1109d6820116a
Nick King <nickmaxking@gmail.com>
2021-04-22 12:18:26 -0600

    Add show and random controller actions

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -1,5 +1,18 @@
 class DiscoverController < ApplicationController
+  before_action :authenticate
+
   def index
 
+  end
+
+  def show
+
+  end
+
+  def random
+    @movie = DiscoverFacade.find_random_movie(current_user.id)
+    @params = {random: true}
+
+    render :show
   end
 end
