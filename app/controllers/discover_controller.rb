b19e49ee1de4eca1985d12b13190c66ea1679c15
Nick King <nickmaxking@gmail.com>
2021-04-23 13:08:26 -0600

    Move swipe create action into discover controller as swipe action

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -15,4 +15,20 @@ class DiscoverController < ApplicationController
 
     render :show
   end
+
+  def swipe
+    swipe = BackendService.create_swipe(swipe_params.merge(user_id: current_user.id))
+
+    if params[:random] = true
+      redirect_to discover_random_path
+    else
+      # future iteration
+    end
+  end
+
+  private
+
+  def swipe_params
+    params.permit(:rating, :movie_id)
+  end
 end
