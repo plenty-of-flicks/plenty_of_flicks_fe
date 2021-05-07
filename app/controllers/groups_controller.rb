2474d8518ae17518504c05127887e696ce21263e
Nick King <nickmaxking@gmail.com>
2021-05-07 15:46:28 -0600

    Add new, create and show actions

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,10 +3,40 @@ class GroupsController < ApplicationController
 
   def index
     @groups = UserFacade.top_three_groups(current_user.id)
-    # @friends = UserFacade.all_friends(current_user.id)
+  end
+
+  def show
+    @group = GroupFacade.find_group(params[:id])
   end
 
   def all
     @groups = UserFacade.all_groups(current_user.id)
+  end
+
+  def new
+    @friends = UserFacade.all_friends(current_user.id)
+  end
+
+  def create
+    user_ids = user_ids(params)
+    group = GroupFacade.make_group(user_ids, group_params[:name])
+
+    redirect_to "/groups/#{group.id}", notice: "#{group.name} was successfully created."
+  end
+
+  private
+
+  def user_ids(params)
+    friends = params.select do |key, value|
+      value == "1"
+    end
+    ids = friends.keys.map do |id|
+      id.to_i
+    end
+    ids << current_user.id
+  end
+
+  def group_params
+    params.permit(:name)
   end
 end
