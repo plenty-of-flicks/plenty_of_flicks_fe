be07d2fbc2f955aa2b9d6c1749f7968ec28c1e1b
Nick King <nickmaxking@gmail.com>
2021-05-07 01:21:47 -0600

    Add test for groups all link and view

--- spec/features/groups/index_spec.rb
+++ spec/features/groups/index_spec.rb
@@ -33,6 +33,48 @@ describe 'groups index page' do
     expect(page).to have_content('You currently have no groups :(')
   end
 
+  it 'view all groups link shows all groups of user', :vcr do
+    stub_omniauth
+    json_response = JSON.parse(File.read('spec/fixtures/groups/index/groups_four_groups.json'), symbolize_names:true)
+    group_list_path = ENV['POF_BE'] + '/api/v1/users/1/groups'
+    stub_request(:get, group_list_path).to_return(status: 200, body: json_response)
+
+    visit google_login_path
+
+    visit groups_path
+
+    click_link 'View All Groups'
+
+    within '.groupslist' do
+      expect(page).to have_content('Scrantonicity')
+      expect(page).to have_content('X Men')
+      expect(page).to have_content('Fantastic Four')
+      expect(page).to have_content('The Avengers')
+      expect(page).to have_content('Nick King')
+      expect(page).to have_content('Ron Swanson')
+    end
+  end
+
+  it 'create group button redirects to group new page', :vcr do
+    stub_omniauth
+    groups_json_response = JSON.parse(File.read('spec/fixtures/groups/index/groups_no_groups.json'), symbolize_names:true)
+    groups_list_path = ENV['POF_BE'] + '/api/v1/users/1/groups'
+    stub_request(:get, groups_list_path).to_return(status: 200, body: groups_json_response)
+
+    friends_json_response = JSON.parse(File.read('spec/fixtures/friends/index/friendslist_four_friends.json'), symbolize_names:true)
+    friendslist_path = ENV['POF_BE'] + '/api/v1/users/1/friends'
+    stub_request(:get, friendslist_path).to_return(status: 200, body: friends_json_response)
+
+
+    visit google_login_path
+
+    visit groups_path
+
+    click_link 'Create a New Group'
+
+    binding.pry
+  end
+
   def stub_omniauth
     OmniAuth.config.test_mode = true
     OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
