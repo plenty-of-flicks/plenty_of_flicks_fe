f81661e2968ae041630a6a8526dfce324261e4cf
Nick King <nickmaxking@gmail.com>
2021-05-09 00:18:24 -0600

    Add test for group show movie names linking to movie show pages

--- spec/features/groups/show_spec.rb
+++ spec/features/groups/show_spec.rb
@@ -36,6 +36,30 @@ describe 'groups show page' do
     expect(page).not_to have_content('The Theory of Everything')
   end
 
+  it 'movie names link to movie show', :vcr do
+    stub_omniauth
+
+    json_response = JSON.parse(File.read('spec/fixtures/groups/index/groups.json'), symbolize_names:true)
+    group_list_path = ENV['POF_BE'] + '/api/v1/users/1/groups'
+    stub_request(:get, group_list_path).to_return(status: 200, body: json_response)
+
+    x_men_response = JSON.parse(File.read('spec/fixtures/groups/show/x_men.json'), symbolize_names:true)
+    x_men_show_path = ENV['POF_BE'] + '/api/v1/groups/159'
+    stub_request(:get, x_men_show_path).to_return(status: 200, body: x_men_response)
+
+    x_men_matches_response = JSON.parse(File.read('spec/fixtures/groups/show/x_men_matches.json'), symbolize_names:true)
+    x_men_matches_path = ENV['POF_BE'] + '/api/v1/groups/159/matches'
+    stub_request(:get, x_men_matches_path).to_return(status: 200, body: x_men_matches_response)
+
+    visit google_login_path
+
+    visit group_path(159)
+
+    click_link 'Austin Powers: International Man of Mystery (1997)'
+
+    expect(current_path).to eq("/movies/480")
+  end
+
   def stub_omniauth
     OmniAuth.config.test_mode = true
     OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
